c00298b175792bef5685b39eb09ccf5df70fc9d1
Kraskaska <k.krasilnikov.2008@gmail.com>
2021-02-27 20:27:22 +0300

    Enhanced ItemsCsv, removed not needed operations

--- src/one/ItemsCsv.java
+++ src/one/ItemsCsv.java
@@ -10,13 +10,12 @@ public class ItemsCsv {
     public static Map<Integer, String> itemsById = new HashMap<Integer, String>();
     public static Map<String, Integer> idByItems = new HashMap<String, Integer>();
     static {
-        String data = new BufferedReader(new InputStreamReader(ItemsCsv.class.getClassLoader().getResourceAsStream("items.csv"))).lines().collect(Collectors.joining("\n"));
-        for (String line : data.split("\n")) {
+        new BufferedReader(new InputStreamReader(ItemsCsv.class.getClassLoader().getResourceAsStream("items.csv"))).lines().forEach(line -> {
             String[] splitten = line.split(",");
             int id = Integer.parseInt(splitten[0]);
             String name = splitten[1];
             itemsById.put(id, name);
             idByItems.put(name, id);
-        }
+        });
     }
 }
