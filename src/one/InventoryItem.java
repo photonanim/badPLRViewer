bcd088e460e4b81845f401296f72ff2cfa05f9ec
Kraskaska <k.krasilnikov.2008@gmail.com>
2021-02-27 19:23:06 +0300

    InventoryItem extends Item

--- src/one/InventoryItem.java
+++ src/one/InventoryItem.java
@@ -1,64 +1,24 @@
 package one;
 
-public class InventoryItem {
+public class InventoryItem extends Item {
+    private boolean favorited;
 
-	private int id, quantity, prefixID;
-	private boolean favorited;
+    // Constructor
+    public InventoryItem(int itemID, int quant, int prefix, boolean fav) {
+        super(itemID, quant, prefix);
+        favorited = fav;
+    }
 
-	// Constructor
-	public InventoryItem(int itemID, int quant, int prefix, boolean fav) {
-		
-		id = itemID;
-		quantity = quant;
-		prefixID = prefix;
-		favorited = fav;
-		
-	}
-	
-	public int getID() {
-		
-		return this.id;
-		
-	}
-	
-	public int getQuantity() {
-		
-		return this.quantity;
-		
-	}
-	
-	public int getPrefixID() {
-		
-		return this.prefixID;
-	}
-	
-	public boolean getFavorited() {
-		
-		return this.favorited;
-		
-	}
-	
-	public void setID(int set) {
-		
-		this.id = set;
-		
-	}
-	
-	public void setQuantity (int set) {
-		
-		this.quantity = set;
-	}
-	
-	public void setPrefixID(int set) {
-		
-		this.prefixID = set;
-		
-	}
-	
-	public void setFavorited(boolean set) {
-		
-		this.favorited = set;
-		
-	}
-	
+    public boolean getFavorited() {
+
+        return this.favorited;
+
+    }
+
+    public void setFavorited(boolean set) {
+
+        this.favorited = set;
+
+    }
+
 }
