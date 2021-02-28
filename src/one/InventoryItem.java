af0afb1c536ede3bed8558fa56e137354b24c1c1
Photon <75060260+photonanim@users.noreply.github.com>
2021-02-28 14:49:00 -0800

    Extending from item
    0.5% less bad

--- src/one/InventoryItem.java
+++ src/one/InventoryItem.java
@@ -1,57 +1,20 @@
 package one;
 
-public class InventoryItem {
+public class InventoryItem extends Item{
 
-	private int id, quantity, prefixID;
 	private boolean favorited;
 
 	// Constructor
 	public InventoryItem(int itemID, int quant, int prefix, boolean fav) {
 		
-		id = itemID;
-		quantity = quant;
-		prefixID = prefix;
+		super(itemID, quant, prefix);
 		favorited = fav;
 		
-	}
-	
-	public int getID() {
-		
-		return this.id;
-		
-	}
-	
-	public int getQuantity() {
-		
-		return this.quantity;
-		
-	}
-	
-	public int getPrefixID() {
-		
-		return this.prefixID;
 	}
 	
 	public boolean getFavorited() {
 		
 		return this.favorited;
-		
-	}
-	
-	public void setID(int set) {
-		
-		this.id = set;
-		
-	}
-	
-	public void setQuantity (int set) {
-		
-		this.quantity = set;
-	}
-	
-	public void setPrefixID(int set) {
-		
-		this.prefixID = set;
 		
 	}
 	
